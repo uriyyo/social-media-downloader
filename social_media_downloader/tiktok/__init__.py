16853f35f8e7f1a47970e8192c9c8bfd30654bd3
Yurii Karabas <1998uriyyo@gmail.com>
2024-03-29 20:42:42 +0200

    Allow to skip video resolving

--- social_media_downloader/tiktok/__init__.py
+++ social_media_downloader/tiktok/__init__.py
@@ -1,6 +1,7 @@
-from .resolver import tiktok_all_links, tiktok_resolve_links
+from .resolver import tiktok_all_links, tiktok_is_video, tiktok_resolve_links
 
 __all__ = [
+    "tiktok_is_video",
     "tiktok_all_links",
     "tiktok_resolve_links",
 ]
--- social_media_downloader/tiktok/resolver.py
+++ social_media_downloader/tiktok/resolver.py
@@ -55,6 +55,7 @@ async def _handle_images(
     client: AsyncClient,
     response: Response,
     *,
+    resolve_image_video: bool = False,
     images_as_video: bool = False,
 ) -> VideoMedia | list[AnyMedia]:
     root = BeautifulSoup(response.text, "html.parser")
@@ -63,6 +64,14 @@ async def _handle_images(
     audio = verify(root.select_one(".music.download_link")).attrs["href"]
 
     token = verify(root.select_one('input[name="slides_data"]')).attrs["value"]
+
+    base: list[AnyMedia] = [
+        *[ImageMedia(url=image) for image in images],
+        AudioMedia(url=audio),
+    ]
+
+    if not resolve_image_video:
+        return base
 
     response = await client.post(
         "https://r.ssstik.top/index.sh",
@@ -80,11 +89,7 @@ async def _handle_images(
     )
 
     if not images_as_video:
-        return [
-            res,
-            *[ImageMedia(url=image) for image in images],
-            AudioMedia(url=audio),
-        ]
+        return [res, *base]
 
     return res
 
@@ -94,6 +99,7 @@ async def _find_links(
     url: str,
     *,
     images_as_video: bool,
+    resolve_image_video: bool,
     add_thumbnail: bool = False,
 ) -> VideoMedia | list[AnyMedia]:
     response = await client.get("https://ssstik.io/en")
@@ -114,7 +120,12 @@ async def _find_links(
     verify(response.content)
 
     if 'id="slides_generate"' in response.text:
-        return await _handle_images(client, response, images_as_video=images_as_video)
+        return await _handle_images(
+            client,
+            response,
+            images_as_video=images_as_video,
+            resolve_image_video=resolve_image_video,
+        )
 
     media = await _handle_video(client, response, add_thumbnail=add_thumbnail)
     return media if images_as_video else [media]
@@ -126,6 +137,7 @@ async def tiktok_resolve_links(
     *,
     images_as_video: Literal[True] = True,
     add_thumbnail: bool = False,
+    resolve_image_video: bool = False,
     client: AsyncClient | None = None,
 ) -> VideoMedia:
     pass
@@ -137,6 +149,7 @@ async def tiktok_resolve_links(
     *,
     images_as_video: Literal[False],
     add_thumbnail: bool = False,
+    resolve_image_video: bool = False,
     client: AsyncClient | None = None,
 ) -> list[AnyMedia]:
     pass
@@ -146,6 +159,7 @@ async def tiktok_resolve_links(
     url: str,
     *,
     images_as_video: bool = True,
+    resolve_image_video: bool = False,
     add_thumbnail: bool = False,
     client: AsyncClient | None = None,
 ) -> VideoMedia | list[AnyMedia]:
@@ -154,11 +168,25 @@ async def tiktok_resolve_links(
             client,
             url,
             images_as_video=images_as_video,
+            resolve_image_video=resolve_image_video,
             add_thumbnail=add_thumbnail,
         )
 
 
+async def tiktok_is_video(
+    url: str,
+    *,
+    client: AsyncClient | None = None,
+) -> bool:
+    async with httpx_client(client) as client:
+        response = await client.get(url)
+        response.raise_for_status()
+
+        return "/video/" in response.url.path
+
+
 __all__ = [
+    "tiktok_is_video",
     "tiktok_all_links",
     "tiktok_resolve_links",
 ]
